e78dca6a7a73cc1b9a70680625fb7d5288502b3b
Kathryn Linehan <kjl5t@virginia.edu>
2022-12-14 11:52:40 -0500

    typo fix

--- test_file_ext.py
+++ test_file_ext.py
@@ -71,7 +71,7 @@ if __name__ == "__main__":
     #print(req_cols)
 
     report = evaluate_folder("./repos")
-    time_checked = datetime.now().strftime("%YY-%m-%d %H:%M:%S")
+    time_checked = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
     print(time_checked)
 
